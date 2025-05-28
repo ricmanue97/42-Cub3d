59d110fd5c9f1643ea2de0f04af32455fc1e9f46
Diogo Alexandre Carvalho Santos <dicarval@c1r10s7.42lisboa.com>
2025-05-28 16:23:16 +0100

    adding stuff

---
+++ cube/window_init.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   window_init.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dicarval <dicarval@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/27 17:01:52 by dicarval          #+#    #+#             */
+/*   Updated: 2025/05/27 17:38:58 by dicarval         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cube.h"
+
+int	window_init(void)
+{
+	cube()->mlx = mlx_init();
+	if (!cube()->mlx)
+		return (UNSUCCESS);
+	cube()->win = mlx_new_window(cube()->mlx, scWIDTH, scHEIGHT, "CONTINENTE");
+	if (!cube()->win)
+		return (UNSUCCESS);
+	if (!load_sprites(cube()));
+		return (UNSUCCESS);
+	if (!game_loop())
+		return (UNSUCCESS);
+	else
+		return (SUCCESS);
+}
+
