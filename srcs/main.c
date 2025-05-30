4e1895cf02ecb1f3b8cd2452ff1cb54e83d7ee71
Diogo Alexandre Carvalho Santos <dicarval@c1r10s11.42lisboa.com>
2025-05-30 15:26:28 +0100

    Beginning the key press

--- srcs/main.c
+++ srcs/main.c
@@ -2,17 +2,32 @@
 
 #include "../includes/cube.h"
 
+void	var_init()
+{
+			//definir a direcao do player aqui
+}
+
+int	close_window(t_game *g)
+{
+	exit(0);
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
-	t_game		game;
-	t_map		map;
-	t_player	player;
-
+	t_game *g;
 
 	if (ac == 2)
 	{
 		ft_args_validation(av);
-
+		var_init();
+		window_init();
+		if (!game_loop())
+			return (UNSUCCESS);
+		g = cube();
+		mlx_hook(g->win, 2, 1L<<0, key_press, g);
+		mlx_hook(g->win, 17, 0, close_window, g);
+		mlx_loop(g->mlx)
 	}
 	else
 	{
